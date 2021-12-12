6d99e8261902bbc5cb66ec08f1e6379a6d21de21
Daniel Ohuruogu <daniel.ohuruogu@hotmail.co.uk>
2021-12-12 21:23:23 +0000

    initial commit

---
+++ src/main/java/com/shopreview/app/RestaurantReviewAppApplication.java
@@ -0,0 +1,13 @@
+package com.shopreview.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantReviewAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantReviewAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shopreview/app/RestaurantReviewAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopreview.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantReviewAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
